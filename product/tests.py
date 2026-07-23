4612d87c5f6c5dbcea631cf58dc7c8e451a7bb93
juniorFandom <juniorfandom95@gmail.com>
2026-07-23 18:20:47 +0100

    doc: update test file

--- product/tests.py
+++ product/tests.py
@@ -1,50 +1,106 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
 class ProductAPITestCase(TestCase):
+
     def test_product_list(self):
-        response = self.client.get('/products/')
+        url = reverse('product-list')
+
+        response = self.client.get(url)
+
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), list)
 
+
     def test_product_create(self):
+        url = reverse('product-create')
+
         data = {
             "name": "Test Product",
             "prix": 9.99,
             "is_active": True
         }
-        response = self.client.post('/products/create/', data, content_type='application/json')
+
+        response = self.client.post(
+            url,
+            data,
+            content_type='application/json'
+        )
+
         self.assertEqual(response.status_code, 201)
         self.assertIn('id', response.json())
 
+
     def test_product_update(self):
-        # First, create a product to update
+
+        create_url = reverse('product-create')
+
         create_data = {
             "name": "Product to Update",
             "prix": 19.99,
             "is_active": True
         }
-        create_response = self.client.post('/products/create/', create_data, content_type='application/json')
-        product_id = create_response.json()['id']
-        
+
+        create_response = self.client.post(
+            create_url,
+            create_data,
+            content_type='application/json'
+        )
+
+        self.assertEqual(create_response.status_code, 201)
+
+        product_slug = create_response.json()['slug']
+
+        update_url = reverse(
+            'product-update',
+            kwargs={'slug': product_slug}
+        )
+
         update_data = {
             "name": "Updated Product",
             "prix": 29.99,
             "is_active": False
         }
-        response = self.client.put(f'/products/{product_id}/update/', update_data, content_type='application/json')
+
+        response = self.client.put(
+            update_url,
+            update_data,
+            content_type='application/json'
+        )
+
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['name'], "Updated Product")
+        self.assertEqual(
+            response.json()['name'],
+            "Updated Product"
+        )
+
 
     def test_product_delete(self):
-        # First, create a product to delete
+
+        create_url = reverse('product-create')
+
         create_data = {
             "name": "Product to Delete",
             "prix": 15.99,
             "is_active": True
         }
-        create_response = self.client.post('/products/create/', create_data, content_type='application/json')
-        product_id = create_response.json()['id']
-        
-        response = self.client.delete(f'/products/{product_id}/delete/')
+
+        create_response = self.client.post(
+            create_url,
+            create_data,
+            content_type='application/json'
+        )
+
+        self.assertEqual(create_response.status_code, 201)
+
+        product_slug = create_response.json()['slug']
+
+        delete_url = reverse(
+            'product-delete',
+            kwargs={'slug': product_slug}
+        )
+
+        response = self.client.delete(delete_url)
+
         self.assertEqual(response.status_code, 204)
